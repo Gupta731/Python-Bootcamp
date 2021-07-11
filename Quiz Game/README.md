ce4f30829763493faf56133e68bb1d96f5515f99
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2021-07-11 11:28:36 +0530

    Quiz Game

---
+++ Quiz Game/README.md
@@ -0,0 +1,4 @@
+# Quiz_Game
+Quiz game made using Python. Concepts used: OOP, functions, modules, dictionaries, etc.
+Sample Output:
+![image](https://user-images.githubusercontent.com/25523043/122340360-ce9a6600-cf5f-11eb-8940-075a32d121ed.png)
---
+++ Quiz Game/quiz_brain.py
@@ -0,0 +1,23 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        response = input(f"Q.{self.question_number}: {current_question.text} (True/False): ").title()
+        self.check_answer(response, current_question.answer)
+
+    def still_has_question(self):
+        return self.question_number < len(self.question_list)
+
+    def check_answer(self, response, correct_answer):
+        if response == correct_answer:
+            print('You got it right!')
+            self.score += 1
+        else:
+            print("That's wrong!")
+        print(f'The correct answer was: {correct_answer}')
+        print(f'Your current score is: {self.score}/{self.question_number}\n')
