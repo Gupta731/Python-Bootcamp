fb05aa45f7b623ff6ccbc42825eb7bdd8b94bf98
Sourabh Gupta <sourabh.gupta.731@gmail.com>
2021-08-12 09:23:27 +0530

    Data Entry Job Automation

---
+++ Data Entry Job Automation/main.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chromium.service import ChromiumService
+from selenium.webdriver.common.by import By
+from scrap_listings import GetListings
+from time import sleep
+
+FORMS_URL = 'https://forms.gle/7irrwBxHUxDSBB63A'
+EDGE_DRIVER_PATH = 'C:/Edge Driver/msedgedriver.exe'
+
+
+class DataEntry:
+    def __init__(self):
+        self.s = ChromiumService(EDGE_DRIVER_PATH, start_error_message='error')
+        self.driver = webdriver.ChromiumEdge(service=self.s)
+        self.driver.maximize_window()
+
+    def submit_data(self):
+        for data in property_data:
+            self.driver.get(FORMS_URL)
+            sleep(3)
+            address_input = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/'
+                                                               'div[2]/div/div[1]/div/div[1]/input')
+            address_input.send_keys(data[0])
+            price_input = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/'
+                                                             'div[2]/div/div[1]/div/div[1]/input')
+            price_input.send_keys(data[1])
+            link_input = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/'
+                                                            'div[2]/div/div[1]/div/div[1]/input')
+            link_input.send_keys(data[2])
+            sleep(1)
+            submit_button = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div/'
+                                                               'span')
+            submit_button.click()
+            sleep(1)
+        self.driver.close()
+
+
+listing = GetListings()
+property_data = listing.get_listing_details()
+data_entry = DataEntry()
+data_entry.submit_data()
---
+++ Data Entry Job Automation/scrap_listings.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from nltk import flatten
+
+ZILLOW_URL = 'https://bit.ly/2VDzfb2'
+ZILLOW_HEADERS = {
+    'User-Agent': 'Defined',
+    'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8',
+}
+
+
+class GetListings:
+    def __init__(self):
+        self.response = requests.get(url=ZILLOW_URL, headers=ZILLOW_HEADERS)
+        self.response.raise_for_status()
+        self.soup = BeautifulSoup(self.response.text, 'lxml')
+        self.property_listings = []
+
+    def get_listing_details(self):
+        listing_address = [item.text for item in self.soup.select(selector='.photo-cards_short li address')]
+        listing_price = flatten([re.findall(r"\$[\w]+,[\w]+", item.text) for item in
+                                 self.soup.select(selector='.photo-cards_short li .list-card-price')])
+        listing_link = [item['href'] for item in self.soup.select(selector='.list-card-info a')]
+        for n, link in enumerate(listing_link):
+            if re.search(r"https://[-?_=./\w]+", link) is None:
+                listing_link[n] = 'https://www.zillow.com' + link
+
+        for item in zip(listing_address, listing_price, listing_link):
+            self.property_listings.append(item)
+        return self.property_listings
